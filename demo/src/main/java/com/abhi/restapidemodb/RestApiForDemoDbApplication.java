b4d1cea8aa3605da49aec8055fd68498f847964d
Abhinaw Tripathi <abhinawtripathi34@gmail.com>
2021-05-08 10:32:51 +0530

    Add files via upload
    INitial commit

---
+++ demo/src/main/java/com/abhi/restapidemodb/RestApiForDemoDbApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.restapidemodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForDemoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiForDemoDbApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/abhi/restapidemodb/RestApiForDemoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.restapidemodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForDemoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
